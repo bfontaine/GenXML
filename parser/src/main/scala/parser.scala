0b7d5926354e95cb5ff4b8c682f9e01b66d86172
Baptiste Fontaine <batifon@yahoo.fr>
2014-11-27 10:23:17 +0100

    javadoc + cleaner code

--- parser/src/main/scala/parser.scala
+++ parser/src/main/scala/parser.scala
@@ -1,4 +1,4 @@
-package edu.parisdiderot.m2.xml.galichetfontaine
+package org.genxml
 
 import scala.collection.JavaConverters._
 import scala.xml._
@@ -11,33 +11,50 @@ object GenParser {
 
   val idLimitersRegExp = "(^@|@$)".r
 
+  /**
+   * Convert a GEDCOM id into a string. It currently only removes leading and
+   * trailing @'s.
+   * @param k the id
+   **/
   def idToString(k : String) =
     idLimitersRegExp.replaceAllIn(k, "")
 
+  /**
+   * Return a string describing an individual event type, usable as an XML tag
+   * @param t the event type
+   **/
   def eventTypeToString(t : IndividualEventType) =
     t.display.toLowerCase
 
+  /**
+   * Convert a GEDCOM document into an XML element
+   * @param g the GEDCOM document, usually obtained by parsing a file
+   **/
   def makeDocumentXML(g : Gedcom) =
     <document>
       <infos></infos>
       <individuals>
         {
-          g.individuals.asScala.map { case (k, indi) =>
-            makeIndividualXML(idToString(k), indi)
+          g.individuals.asScala.map { case (_, indi) =>
+            makeIndividualXML(indi)
           }
         }
       </individuals>
       <families>
         {
-          g.families.asScala.map { case (k, fam) =>
-            makeFamilyXML(idToString(k), fam)
+          g.families.asScala.map { case (_, fam) =>
+            makeFamilyXML(fam)
           }
         }
       </families>
     </document>
 
-  def makeIndividualXML(key : String, individual : Individual) =
-    <individual id={key}>
+  /**
+   * Convert an individual document into an XML element
+   * @param individual the GEDCOM individual
+   **/
+  def makeIndividualXML(individual : Individual) =
+    <individual id={idToString(individual.xref)}>
       <!-- TODO name, death, etc -->
       <sex>{ individual.sex }</sex>
       { makeEventXML(BIRTH, individual.events) }
@@ -49,7 +66,7 @@ object GenParser {
       { makeStringsListXML("phone", individual.phoneNumbers) }
       { makeAddressXML(individual.address) }
       <notes>{
-        individual.notes.asScala.map((n) =>
+        individual.notes.asScala.map(n =>
           <note>
             { n.lines.asScala.mkString("\n") }
           </note>
@@ -57,11 +74,19 @@ object GenParser {
       }</notes>
     </individual>
 
-  def makeFamilyXML(key : String, family : Family) =
-    <family id={key}>
+  /**
+   * Convert a family document into an XML element
+   * @param family the GEDCOM family
+   **/
+  def makeFamilyXML(family : Family) =
+    <family id={idToString(family.xref)}>
       <!-- TODO -->
     </family>
 
+  /**
+   * Convert a GEDCOM address into an XML element
+   * @param addr the GEDCOM address
+   **/
   def makeAddressXML(addr : Address) =
     if (addr != null)
       <address>
@@ -73,6 +98,12 @@ object GenParser {
         <country>{ addr.country }</country>
       </address>
 
+  /**
+   * Convert an individual event into an XML element. It takes a type and a
+   * list, and use the first element of this type.
+   * @param type_ the event type we're interested in
+   * @param events the individual's events list
+   **/
   def makeEventXML(type_ : IndividualEventType,
                    events : java.util.List[IndividualEvent]) =
     events.asScala.find(_.`type` == type_) match {
@@ -94,13 +125,20 @@ object GenParser {
    *        <foo>bar</foo>
    *        <foo>qux</foo>
    *      </foos>
+   * @param baseTag the tag used for the list. It'll be used for each inner
+   * element and pluralized for the container element.
+   * @param lst the string list
    **/
   def makeStringsListXML(baseTag : String,
                          lst : java.util.List[StringWithCustomTags]) =
     <xml>{
-      lst.asScala.map((s) => <xml>{s}</xml>.copy(label=baseTag))
+      lst.asScala.map(s => <xml>{s}</xml>.copy(label=baseTag))
     }</xml>.copy(label = baseTag + "s")
 
+  /**
+   * Parse a file and return a GEDCOM document
+   * @param filename
+   **/
   def parseFile(filename : String) = {
     val gp = new GedcomParser()
     gp.load(filename)
@@ -109,6 +147,7 @@ object GenParser {
 
   /**
    * Read a GEDCOM file and return an XML string
+   * @param filename
    **/
   def convertFile(filename : String) = {
     val xml = makeDocumentXML(parseFile(filename))
