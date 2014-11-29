ca1f90f9284da81ca194ae86bc6cf818d9e79f33
Baptiste Fontaine <batifon@yahoo.fr>
2014-11-30 00:08:15 +0100

    old code removed, replaced with OO one

--- parser/src/main/scala/model.scala
+++ parser/src/main/scala/model.scala
@@ -18,6 +18,13 @@ object GedcomConverters {
     def toXML : Elem
   }
 
+  trait XMLCustomTag extends XMLable {
+    def toXML(tag : String) : Elem =
+      (this.toXML : Elem).copy(label=tag)
+  }
+
+  /** Helpers **/
+
   implicit class StringId(val id : String) {
     private val idLimitersRegExp = "(^@|@$)".r
 
@@ -25,17 +32,32 @@ object GedcomConverters {
       idLimitersRegExp.replaceAllIn(id, "")
   }
 
-  implicit class GenEventType(val t : IndividualEventType) {
-    def title = t.display.toLowerCase
-  }
+  /** Document **/
 
-  implicit class GenDocument(val g : Gedcom) extends XMLable {
+  implicit class GDocument(val g : Gedcom) extends XMLable {
     def toXML =
       <document>
-        <infos></infos>
+        <infos>TODO</infos>
         { g.individuals.toXML }
         { g.families.toXML }
       </document>
+  }
+
+  /** Individuals **/
+
+  implicit class GIndividual(val individual : Individual) extends XMLable {
+    def toXML =
+      <individual id={individual.xref.toStringId}>
+        <!-- TODO name, death, etc -->
+        <sex>{ individual.sex }</sex>
+        { individual.events.toXML }
+        { individual.emails.toXML("email") }
+        { individual.faxNumbers.toXML("faxNumber") }
+        { individual.wwwUrls.toXML("url") }
+        { individual.phoneNumbers.toXML("phone") }
+        { if (individual.address != null) individual.address.toXML }
+        { individual.notes.toXML }
+      </individual>
   }
 
   implicit class GenIndividualsList(val indivs : JMap[String, Individual]) extends XMLable {
@@ -45,6 +67,15 @@ object GedcomConverters {
       </individuals>
   }
 
+  /** Families **/
+
+  implicit class GFamily(val family : Family) extends XMLable {
+    def toXML = // TODO
+      <family id={family.xref.toStringId}>
+        <!-- TODO -->
+      </family>
+  }
+
   implicit class GenFamiliesList(val fams : JMap[String, Family]) extends XMLable {
     def toXML =
       <families>
@@ -52,40 +83,20 @@ object GedcomConverters {
       </families>
   }
 
-  implicit class GenIndividual(val individual : Individual) extends XMLable {
+  /** Events **/
+
+  implicit class GIndividualEvent(val ev : IndividualEvent) extends XMLable {
     def toXML =
-      <individual id={individual.xref.toStringId}>
-        <!-- TODO name, death, etc -->
-        <sex>{ individual.sex }</sex>
-        { individual.events.toXML }
-        { individual.emails.toXML }
-        { individual.faxNumbers.toXML }
-        { individual.wwwUrls.toXML }
-        { individual.phoneNumbers.toXML }
-        { individual.address.toXML }
-        { individual.notes.toXML }
-      </individual>
+      <event type={ev.`type`.title}>
+          <date>{ev.date}</date>
+          { if (ev.cause != null) <cause>{ev.cause}</cause> }
+          { if (ev.place != null) <place>{ev.place.placeName}</place> }
+          <!-- TODO -->
+      </event>
   }
 
-  implicit class GenFamily(val family : Family) extends XMLable {
-    def toXML = // TODO
-      <family id={family.xref.toStringId}>
-        <!-- TODO -->
-      </family>
-  }
-
-  implicit class GenAddress(val addr : Address) extends XMLable {
-    def toXML =
-      if (addr != null)
-        <address>
-          <addr1>{ addr.addr1 }</addr1>
-          <addr2>{ addr.addr2 }</addr2>
-          <postalCode>{ addr.postalCode }</postalCode>
-          <city>{ addr.city }</city>
-          <stateOrProvince>{ addr.stateProvince }</stateOrProvince>
-          <country>{ addr.country }</country>
-        </address>
-      else <address/>
+  implicit class GIndividualEventType(val t : IndividualEventType) {
+    def title = t.display.toLowerCase
   }
 
   implicit class GenIndividualEvents(val evs : JList[IndividualEvent]) extends XMLable {
@@ -95,9 +106,59 @@ object GedcomConverters {
       </events>
   }
 
-  implicit class GenIndividualEvent(val ev : IndividualEvent) extends XMLable {
+  /** Notes **/
+
+  implicit class GNote(val note : Note) extends XMLable {
     def toXML =
-      <event></event>
+      <note>{ note.lines.asScala.mkString("\n") }</note>
+  }
+
+  implicit class GNotes(val notes : JList[Note]) extends XMLable {
+    def toXML =
+      <notes>
+        { notes.asScala.map(_.toString) }
+      </notes>
+  }
+
+  /** Individual fields **/
+
+  implicit class GAddress(val addr : Address) extends XMLable {
+    def toXML =
+      <address>
+        <addr1>{ addr.addr1 }</addr1>
+        <addr2>{ addr.addr2 }</addr2>
+        <postalCode>{ addr.postalCode }</postalCode>
+        <city>{ addr.city }</city>
+        <stateOrProvince>{ addr.stateProvince }</stateOrProvince>
+        <country>{ addr.country }</country>
+      </address>
+  }
+
+  /** Others **/
+
+  implicit class GCustomString(val s : StringWithCustomTags) extends
+  XMLCustomTag {
+    def toXML =
+      <xml>{s}</xml>
+  }
+
+  implicit class GCustomStrings(val ss : JList[StringWithCustomTags]) extends
+  XMLCustomTag {
+    def toXML =
+      this.toXML("string")
+
+    /**
+     * List(StringWithCustomTags("A"), * StringWithCustomTags("B")).toXML("X")
+     * =>
+     *  <Xs>
+     *    <X>A</X>
+     *    <X>B</X>
+     *  </Xs>
+     **/
+    override def toXML(tag : String) =
+      <xml>
+        { ss.asScala.toList.map(_.toXML(tag)) }
+      </xml>.copy(label=tag+"s")
   }
 
 }
--- parser/src/main/scala/parser.scala
+++ parser/src/main/scala/parser.scala
@@ -2,79 +2,11 @@ package org.genxml
 
 import GedcomConverters._
 
-import scala.collection.JavaConverters._
 import scala.xml._
 
 import org.gedcom4j.parser.GedcomParser
-import org.gedcom4j.model._
-import org.gedcom4j.model.IndividualEventType._
 
 object GenParser {
-
-  /**
-   * Convert an individual document into an XML element
-   * @param individual the GEDCOM individual
-   **/
-  def makeIndividualXML(individual : Individual) =
-    individual.toXML
-  /*
-    <individual id={individual.xref.toStringId}>
-      <!-- TODO name, death, etc -->
-      <sex>{ individual.sex }</sex>
-      { makeEventXML(BIRTH, individual.events) }
-      { makeEventXML(DEATH, individual.events) }
-      { makeEventXML(BURIAL, individual.events) }
-      { makeStringsListXML("email", individual.emails) }
-      { makeStringsListXML("faxNumber", individual.faxNumbers) }
-      { makeStringsListXML("url", individual.wwwUrls) }
-      { makeStringsListXML("phone", individual.phoneNumbers) }
-      { individual.address.toXML }
-      <notes>{
-        individual.notes.asScala.map(n =>
-          <note>
-            { n.lines.asScala.mkString("\n") }
-          </note>
-        )
-      }</notes>
-    </individual>
-  */
-
-  /**
-   * Convert an individual event into an XML element. It takes a type and a
-   * list, and use the first element of this type.
-   * @param type_ the event type we're interested in
-   * @param events the individual's events list
-   **/
-  def makeEventXML(type_ : IndividualEventType,
-                   events : java.util.List[IndividualEvent]) =
-    events.asScala.find(_.`type` == type_) match {
-      case Some(ev) =>
-        <xml>
-          <date>{ev.date}</date>
-          { if (type_ == DEATH && ev.cause != null) <cause>{ev.cause}</cause> }
-          { if (ev.place != null) <place>{ev.place.placeName}</place> }
-          <!-- TODO -->
-        </xml>.copy(label=type_.title)
-      case None => ""
-    }
-
-  /**
-   * Return an XML '{baseTag}s' element that contains one '{baseTag}' element
-   * per {lst}'s element. For example:
-   *    makeStringListXML("foo", ("bar", "qux")) =
-   *      <foos>
-   *        <foo>bar</foo>
-   *        <foo>qux</foo>
-   *      </foos>
-   * @param baseTag the tag used for the list. It'll be used for each inner
-   * element and pluralized for the container element.
-   * @param lst the string list
-   **/
-  def makeStringsListXML(baseTag : String,
-                         lst : java.util.List[StringWithCustomTags]) =
-    <xml>{
-      lst.asScala.map(s => <xml>{s}</xml>.copy(label=baseTag))
-    }</xml>.copy(label = baseTag + "s")
 
   /**
    * Parse a file and return a GEDCOM document
@@ -91,10 +23,10 @@ object GenParser {
    * @param filename
    **/
   def convertFile(filename : String) = {
-    val xml = parseFile(filename).toXML
+    val content = parseFile(filename)
     // 90 chars per line, 4-spaces indent
     val printer = new PrettyPrinter(90, 4)
-    printer.format(xml)
+    printer.format(content.toXML)
   }
 
   def main(args : Array[String]) {
@@ -104,6 +36,7 @@ object GenParser {
       System.exit(-1)
     }
 
+    // TODO write the result in a file as well
     args.foreach(convertFile)
   }
 
